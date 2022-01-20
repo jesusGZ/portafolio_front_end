50dcdd8990f0df34949766a2524828e2934745bd
jesusGZ <j.gr.zurita@gmail.com>
2022-01-20 11:16:03 -0600

    paginas

---
+++ src/Pages/Portfolio/Portfolio.js
@@ -0,0 +1,70 @@
+import { CardActionArea, CardContent, CardMedia, Card, Grid, Tab, Tabs, Grow, Typography, Dialog, DialogTitle, DialogContent, DialogActions } from '@material-ui/core';
+import { ImagesGallery } from '../../compontent/ImageGallery/ImageGallery';
+
+import React, { useState } from 'react';
+import resumeData from '../../utilis/resumeData';
+import './Portfolio.css';
+export const Portfolio = () => {
+	const [tabValue, setTabValue] = useState('All');
+	const [projectDialog, setProjectDialog] = useState(false);
+
+	return (
+		<Grid container className="section pb_45 pt_45">
+			<Grid item spacing={1} className="section_title mb_20">
+				<span></span>
+				<h6 className="section_title_text">Portafolio</h6>
+			</Grid>
+			<Grid item xs={12}>
+				<Tabs value={tabValue} indicatorColor="white" className="customTabs" onChange={(event, newValue) => setTabValue(newValue)}>
+					<Tab label="All" value="All" className={tabValue === 'All' ? 'customTabs_item active' : 'customTabs_item'} />
+					{[...new Set(resumeData.projects.map((item) => item.tag))].map((tag) => (
+						<Tab label={tag} value={tag} className={tabValue === tag ? 'customTabs_item active' : 'customTabs_item'} />
+					))}
+				</Tabs>
+			</Grid>
+			<Grid item xs={12}>
+				<Grid container spacing={4}>
+					{resumeData.projects.map((project) => (
+						<>
+							{tabValue == project.tag || tabValue == 'All' ? (
+								<Grid item xs={12} sm={6} md={4}>
+									<Grow in timeout={1000}>
+										<Card className="customCard" onClick={() => setProjectDialog(project)}>
+											<CardActionArea>
+												<CardMedia className="customCard_img" image={project.images[0]} title={project.title} />
+												<CardContent>
+													<Typography variant={'body2'} className="customCard_title">
+														{project.title}
+													</Typography>
+													<Typography variant="body2" className="customCard_caption">
+														{project.caption}
+													</Typography>
+												</CardContent>
+											</CardActionArea>
+										</Card>
+									</Grow>
+								</Grid>
+							) : null}
+						</>
+					))}
+				</Grid>
+			</Grid>
+
+			<Dialog open={projectDialog} className="projectDialog" onClose={() => setProjectDialog(false)} maxWidth={'lg'} fullWidth>
+				<DialogTitle onClose={() => setProjectDialog(false)}>{projectDialog.title}</DialogTitle>
+				{/* <img src={projectDialog.image} alt="" className="projectDialog_img" /> */}
+				<DialogContent style={{ height: '80vh' }}>
+					{projectDialog.images && <ImagesGallery images={projectDialog.images} className="projectDialog_img" />}
+					<Typography className="projectDialog_description">{projectDialog.description}</Typography>
+					<DialogActions className="projectDialog_action">
+						{projectDialog?.links?.map((link) => (
+							<a href={link.link} target="_blank" className="projectDialog_icon" rel="noreferrer">
+								{link.icon}
+							</a>
+						))}
+					</DialogActions>
+				</DialogContent>
+			</Dialog>
+		</Grid>
+	);
+};
---
+++ src/Pages/Resume/Resume.js
@@ -0,0 +1,106 @@
+import { Grid, Paper, Typography } from '@material-ui/core';
+import React from 'react';
+import CustomTimeline, { CustomTimelineSeparator } from '../../compontent/CustomTimeline/CustomTimeline';
+import resumeData from '../../utilis/resumeData';
+import WorkIcon from '@material-ui/icons/Work';
+import SchoolIcon from '@material-ui/icons/School';
+import './Resume.css';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import TimelineSVG from '../../compontent/TimelineSVG/TimelineSVG';
+
+export const Resume = () => {
+	return (
+		<>
+			<Grid container className="section pb_45 pt_45">
+				<Grid item className="section_title mb_30">
+					<span></span>
+					<h6 className="section_title_text">Hacerca de mí</h6>
+				</Grid>
+				<Grid item xs={12}>
+					<Typography className="aboutme_text" variant="body2">
+						{resumeData.about}
+					</Typography>
+				</Grid>
+			</Grid>
+			<Grid container className="section pb_45">
+				<Grid item className="section_title mb_30">
+					<span></span>
+					<h6 className="section_title_text">Preparación</h6>
+				</Grid>
+				<Grid container className="resume_timeline">
+					<Grid item xs={12} sm={6}>
+						<CustomTimeline title="Experiencia Laboral" icon={<WorkIcon />}>
+							{resumeData.experiences.map((exp) => (
+								<TimelineItem>
+									<CustomTimelineSeparator />
+									<TimelineContent className="timeline_content">
+										<Typography className="timeline_title">{exp.title}</Typography>
+										<Typography variant="caption" className="timeline_date">
+											{exp.date}
+										</Typography>
+										<Typography className="timeline_description">{exp.description}</Typography>
+									</TimelineContent>
+								</TimelineItem>
+							))}
+						</CustomTimeline>
+					</Grid>
+					<Grid item xs={12} sm={6}>
+						<CustomTimeline title="Educación" icon={<SchoolIcon />}>
+							{resumeData.education.map((edu) => (
+								<TimelineItem>
+									<CustomTimelineSeparator />
+									<TimelineContent className="timeline_content">
+										<Typography className="timeline_title">{edu.title}</Typography>
+										<Typography variant="caption" className="timeline_date">
+											{edu.date}
+										</Typography>
+										<Typography className="timeline_description">{edu.description}</Typography>
+									</TimelineContent>
+								</TimelineItem>
+							))}
+						</CustomTimeline>
+					</Grid>
+				</Grid>
+			</Grid>
+			<Grid container className="section ">
+				<Grid item className="section_title section">
+					<span></span>
+					<h6 className="section_title_text">Habilidades</h6>
+				</Grid>
+				<Grid container spacing={3} justify="space-between" className="section pb_45 pt_25">
+					{resumeData.skills.map((skill) => (
+						<Grid item xs={12} sm={6} md={3} lg={3}>
+							<Paper elevation={0} className="skill">
+								<Typography variant="h6" className="skill_titles">
+									{skill.title}
+								</Typography>
+								{skill.description.map((element) => (
+									<Typography variant="body2" className="skill_description">
+										<TimelineDot variant={'outlined'} className="timeline_dot" />
+										{element}
+									</Typography>
+								))}
+							</Paper>
+						</Grid>
+					))}
+				</Grid>
+			</Grid>
+			<Grid container className="section pb_10 ">
+				<Grid item className="section_title section ">
+					<span></span>
+					<h6 className="section_title_text ">Cursos</h6>
+				</Grid>
+				<Grid container className="pt_25 ">
+					<Grid item xs={12}>
+						{resumeData.certifications.map((cert) => (
+							<TimelineSVG side={cert.side} title={cert.title} date={cert.date} organization={cert.organization} link={cert.link} />
+						))}
+					</Grid>
+				</Grid>
+			</Grid>
+		</>
+	);
+};
