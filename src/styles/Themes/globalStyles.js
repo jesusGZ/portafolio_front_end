b28cb696fec49cd287beea8c9f0c33da638b719e
jesusGZ <j.gr.zurita@gmail.com>
2022-05-19 08:57:01 -0500

    ordenamiento de codigo

--- src/styles/Themes/globalStyles.js
+++ src/styles/Themes/globalStyles.js
@@ -8,38 +8,30 @@ export const GlobalStyles = createGlobalStyle`
 }
 
   body {
-    margin: 0;
-	font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	background: ${({ theme }) => theme.body};
-	background-color: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
-	overflow-x: hidden;
-    transition: all 0.55s linear;
-	background-image: url(${img});
+      margin: 0;
+	    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+	    -webkit-font-smoothing: antialiased;
+	    -moz-osx-font-smoothing: grayscale;
+	    background: ${({ theme }) => theme.body};
+	    background-color: ${({ theme }) => theme.body};
+      color: ${({ theme }) => theme.text};
+	    overflow-x: hidden;
+      transition: all 0.55s linear;
+	    background-image: url(${img});
   }
 
-  .timeline .timeline_title{
-	color: ${({ theme }) => theme.text};
-  }
+  .timeline .timeline_title{ color: ${({ theme }) => theme.text}; }
 
-  .timeline .timeline_link{
-	color: ${({ theme }) => theme.text};
-  }
+  .timeline .timeline_link{ color: ${({ theme }) => theme.text}; }
 
-  .header .header_link {
-	color: ${({ theme }) => theme.text} !important;
-  }
+  .header .header_link { color: ${({ theme }) => theme.text} !important; }
 
   .MuiPaper-root {
-	background-color: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
-	transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-	background: ${({ theme }) => theme.bag};
+	    background-color: ${({ theme }) => theme.body};
+      color: ${({ theme }) => theme.text};
+	    transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+	    background: ${({ theme }) => theme.bag};
   }
 
-  code {
-	font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
-}
+  code { font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace; }
   `;
